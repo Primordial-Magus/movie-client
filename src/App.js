d0645b925c23d512c6e833f96b5bdb099edf72e0
Eric Tran <erictran0007@gmail.com>
2024-05-14 18:59:40 +1000

    Added Hero and Home components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig';
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Route, Routes} from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/home/Home";
+
 
 function App() {
+
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+
+    try {
+      const response = await axios.get("http://localhost:8080/api/v1/movies")
+      console.log(response.data);
+      setMovies(response.data);
+    }
+    catch(e) {
+        console.error(e.data);
+    }
+
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home movies={movies} />}></Route>
+
+
+        </Route>
+      </Routes>
     </div>
   );
 }

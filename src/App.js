741c303f97d9fc3921a6b32f8aeaf43a741bbc14
Eric Tran <erictran0007@gmail.com>
2024-05-14 19:09:35 +1000

    Updated App.js to enable movie selection and review

--- src/App.js
+++ src/App.js
@@ -1,43 +1,74 @@
 import './App.css';
 import api from './api/axiosConfig';
-import {useEffect, useState} from "react";
-import axios from "axios";
-import {Route, Routes} from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./components/home/Home";
-
+import {useState, useEffect} from 'react';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
 
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
-  const getMovies = async () => {
+  const getMovies = async () =>{
 
-    try {
-      const response = await axios.get("http://localhost:8080/api/v1/movies")
-      console.log(response.data);
+    try
+    {
+
+      const response = await api.get("http://localhost:8080/api/v1/movies");
+
       setMovies(response.data);
+
     }
-    catch(e) {
-        console.error(e.data);
+    catch(err)
+    {
+      console.log(err);
+    }
+  }
+
+  const getMovieData = async (movieId) => {
+
+    try
+    {
+      const response = await api.get(`http://localhost:8080/api/v1/movies/${movieId}`);
+
+      const singleMovie = response.data;
+
+      setMovie(singleMovie);
+
+      setReviews(singleMovie.reviews);
+
+
+    }
+    catch (error)
+    {
+      console.error(error);
     }
 
   }
 
   useEffect(() => {
     getMovies();
-  }, [])
+  },[])
 
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route path="/" element={<Home movies={movies} />}></Route>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<Home movies={movies} />} ></Route>
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+            <Route path="*" element = {<NotFound/>}></Route>
+          </Route>
+        </Routes>
 
-
-        </Route>
-      </Routes>
-    </div>
+      </div>
   );
 }
 
